0f93d9f9cbdd08aa10a11f13510b074f7cf2a316
adema <adema.kuduzovic@netitup.it>
2025-10-07 10:19:27 +0000

    define an action for smart button

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -9,5 +9,15 @@ class EstatePropertyType(models.Model):
     ]
 
     property_ids = fields.One2many('estate.property', 'property_type_id', string='Types')
-    
     name=fields.Char(required=True)
+
+    def action_open_property_ids(self):
+        return {
+            "name": "Related properties",
+            "type": "ir.actions.act.window",
+            "view_mode": "list, form", 
+            "res_model": "estate.property",
+            "target": "current",
+            "domain": [("property_type_id", "=", self.id)],
+            "context": {"default_property_type_id": self.id}
+        }
