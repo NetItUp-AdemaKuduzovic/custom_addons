c75660f9b3b962a4b0bd773b41a221d77de9c6a2
adema <adema.kuduzovic@netitup.it>
2025-10-07 10:56:17 +0000

    override create and unlink methods for estate property type to add property tag an cancel all properites on deletion of property type

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -12,6 +12,20 @@ class EstatePropertyType(models.Model):
     name=fields.Char(required=True)
     property_count = fields.Integer(compute="_compute_property_count")
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list: 
+            self.env['estate.property.tag'].create(
+                {
+                    "name": vals.get("name")
+                }
+            )
+        return super().create(vals_list)
+    
+    def unlink(self):
+        self.property_ids.state = 'cancelled'
+        return super().unlink()
+
     @api.depends("property_ids")
     def _compute_property_count(self):
         for rec in self: 
