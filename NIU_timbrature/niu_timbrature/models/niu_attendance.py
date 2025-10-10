36ad92cdac9477eb8e1251017327e2afca72aad5
adema <adema.kuduzovic@netitup.it>
2025-10-10 16:01:28 +0000

    fix: change attendance_date field type to Date, add some fields, update search logic wip

--- NIU_timbrature/niu_timbrature/models/niu_attendance.py
+++ NIU_timbrature/niu_timbrature/models/niu_attendance.py
@@ -6,7 +6,13 @@ class NiuAttendance(models.Model):
 
     name = fields.Char(string="Attendance Name", required=True)
     active = fields.Boolean(string="Active", default=True)
-    attendance_date = fields.Datetime(string="Attendance Date", required=True)
+    attendance_date = fields.Date(string="Attendance Date", required=True)
+
+    day = fields.Char()
+    first_in = fields.Datetime(string="Morning In")
+    first_out = fields.Datetime(string="Morning Out")
+    second_in = fields.Datetime(string="Afternoon In")
+    second_out = fields.Datetime(string="Afternoon Out")
 
     employee_id = fields.Many2one('hr.employee', string='Employee', required=True)
     niu_stamping_ids = fields.One2many('niu.stamping', 'niu_attendance_id', string='Related Stampings')
@@ -14,8 +20,9 @@ class NiuAttendance(models.Model):
     @api.model
     def _generate_attendances(self):
         # Method called from cron job to generate new attendances every day at midnight, then stampings get populated into them from controller
+        # Make a set of of all attendances, employee for a date, then search if existant, if not create one 
         today = fields.Date.context_today(self)
-        existing_attendances = self.search([('attendance_date', '>=', today), ('attendance_date', '<', today + fields.Date.timedelta(days=1))])
+        existing_attendances = self.search([('attendance_date', '=', today), ('employee_id', '=', employee.id)])
         if not existing_attendances:
             employees = self.env['hr.employee'].search([])
             for employee in employees:
