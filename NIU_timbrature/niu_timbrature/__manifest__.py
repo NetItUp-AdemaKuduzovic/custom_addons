274a986a883c9738d3b2097629858369451558ae
adema <adema.kuduzovic@netitup.it>
2025-10-10 13:31:22 +0000

    inital commit new module for niu attendances

---
+++ NIU_timbrature/niu_timbrature/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "NIU Timbrature - testing",
+    'version': '1.0',
+    'depends': ['base', 'hr'],
+    'author': "Adema Sarajlija",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    'data': [
+        'data/cron.xml',
+        'views/niu_stamping_views.xml',
+        'views/niu_attendance_views.xml',
+        'views/niu_timbrature_menus.xml',
+    ],
+
+    'demo': [
+    ],
+}
